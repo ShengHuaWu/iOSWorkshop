aeb20eb9323d48c6e035073118383956b5e161b5
ShengHua Wu <fantasy0404@gmail.com>
2023-03-25 12:56:57 +0000

    Create modern swift ui project.

---
+++ ModernSwiftUI/ModernSwiftUI/ModernSwiftUI/ModernSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModernSwiftUIApp.swift
+//  ModernSwiftUI
+//
+//  Created by ShengHua Wu on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ModernSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ModernSwiftUI/README.md
@@ -0,0 +1,2 @@
+# Modern SwiftUI
+This is an exercies project of building a SwiftUI app based on [this episode](https://www.pointfree.co/episodes/ep214-modern-swiftui-introduction).
